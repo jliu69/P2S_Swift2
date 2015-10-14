47e3decc34f9b5869f418414753792f3f4ca6906
jliu69 <liu_johnson@yahoo.com>
2015-10-14 18:43:38 -0400

    new files

---
+++ P2SS2/Helper.swift
@@ -0,0 +1,19 @@
+//
+//  Helper.swift
+//  p3P2S
+//
+//  Created by Johnson Liu on 10/11/15.
+//  Copyright © 2015 HomeOffice. All rights reserved.
+//
+
+import UIKit
+
+class Helper: NSObject {
+    
+    func pageBgColor() -> UIColor {
+        
+        let bgColor = UIColor(red: 255.0/255.0, green: 215.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        return bgColor
+    }
+    
+}
---
+++ P2SS2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P2SS2
+//
+//  Created by Johnson Liu on 10/14/15.
+//  Copyright © 2015 HomeOffice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
